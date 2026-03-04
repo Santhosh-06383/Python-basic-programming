96217cd3532d22be31ecc7903559475f4d7fb848
Santhosh kumar S <jyothisathis84@gmail.com>
2026-03-04 23:51:06 +0530

    Added Conditon.py

---
+++ EXP5/Conditon.py
@@ -0,0 +1,11 @@
+s = input("Enter the binary inputs:")
+list1 = s.split(',')
+mylist = []
+
+print(list1)
+
+for x in list1:
+    if int(x, 2) % 5 == 0:
+        mylist.append(x)
+
+print("Divsible by 5: ", mylist)
