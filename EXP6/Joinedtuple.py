8e3eb3630dda35d8225c0d11d78c90a0708a483b
Santhosh kumar S <jyothisathis84@gmail.com>
2026-03-04 23:57:52 +0530

    Added Joinedtuple.py

---
+++ EXP6/Joinedtuple.py
@@ -0,0 +1,9 @@
+m = input("Enter the elements: ")
+tuple1 = tuple(m.split(','))
+
+n = input("Enter the elements: ")
+tuple2 = tuple(n.split(','))
+
+tuple3 = tuple1 + tuple2
+
+print("Joined tuple: ", tuple3)
