aafcba590bbd946184bf90a59c8909fa0303f4c3
Santhosh kumar S <jyothisathis84@gmail.com>
2026-03-04 23:40:02 +0530

    Added stringcomp.py

---
+++ EXP2/stringcomp.py
@@ -0,0 +1,14 @@
+"""
+Experiment 2 - Program 3
+Compare two strings.
+"""
+
+string1 = input("Enter first string: ")
+string2 = input("Enter second string: ")
+
+if string1 == string2:
+    print("Both strings are equal.")
+elif string1 > string2:
+    print("First string is greater.")
+else:
+    print("Second string is greater.")
