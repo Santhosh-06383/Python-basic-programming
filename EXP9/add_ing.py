ea630ee2114e6f30ce40f8edd469c3e9c078434d
Santhosh kumar S <jyothisathis84@gmail.com>
2026-03-05 00:06:49 +0530

    Added add_ing.py

---
+++ EXP9/add_ing.py
@@ -0,0 +1,10 @@
+s = input("enter the string:")
+
+if len(s) >= 3:
+    if s.endswith('ing'):
+        s += 'ly'
+    else:
+        s += 'ing'
+    print(s)
+else:
+    print("The string is less than 3, string is: ", s)
