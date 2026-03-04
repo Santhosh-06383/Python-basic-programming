a2264f631c2425d22fa48ea12185b36caa418a5e
Santhosh kumar S <jyothisathis84@gmail.com>
2026-03-04 23:38:03 +0530

    Added Leapyear.py

---
+++ EXP2/Leapyear.py
@@ -0,0 +1,11 @@
+"""
+Experiment 2 - Program 1
+Check whether a year is a leap year.
+"""
+
+year = int(input("Enter a year: "))
+
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print(f"{year} is a Leap Year.")
+else:
+    print(f"{year} is not a Leap Year.")
